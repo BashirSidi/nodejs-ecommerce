8d586a19f6524c3d5252527fe6126c8c32705920
BashirSidi <bashirsalisusidi@gmail.com>
2021-10-04 15:12:29 +0100

    started working about products

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
   keys: ['shfsdjkgfhjsekgessjkdfg']
 }));
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("Server started...")
